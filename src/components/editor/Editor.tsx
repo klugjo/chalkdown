57dcae7fe6a87a2571dc348421987de9e84d923d
klugjo <klughertz.j@gmail.com>
2017-11-09 19:28:05 +0800

    Add editor.tsx

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,20 +1,52 @@
 import * as React from 'react';
-import {connect} from 'react-redux';
-import {Dispatch} from 'redux';
 
-import * as css from './Editor.css'
+import './Editor.css';
 import core from './core';
+import mdGrammar from './mdGrammar';
+
+const prismjs  = require('prismjs/components/prism-core');
 
 interface IEditorProps {}
 
 interface IEditorState {}
 
-class Editor extends React.Component<IEditorProps, IEditorState> {
+export default class Editor extends React.Component<IEditorProps, IEditorState> {
     private _root: HTMLDivElement;
     private _content: HTMLPreElement;
 
     public componentDidMount() {
+        const editor = core({rootElement: this._root, contentElement: this._content});
 
+        const prismGrammar = mdGrammar({
+            fences: true,
+            tables: true,
+            footnotes: true,
+            abbrs: true,
+            deflists: true,
+            tocs: true,
+            dels: true,
+            subs: true,
+            sups: true
+        });
+
+        editor.init({
+            sectionHighlighter: (section) => {
+                return section.text ? prismjs.highlight(section.text, prismGrammar) : '';
+            },
+            // Optional (increases performance on large documents)
+            sectionParser: (text) => {
+                var offset = 0;
+                var sectionList = [];
+                (text + '\n\n').replace(/^.+[ \t]*\n=+[ \t]*\n+|^.+[ \t]*\n-+[ \t]*\n+|^\#{1,6}[ \t]*.+?[ \t]*\#*\n+/gm,
+                    (match, matchOffset) => {
+                        sectionList.push(text.substring(offset, matchOffset));
+                        offset = matchOffset;
+                        return match;
+                });
+                sectionList.push(text.substring(offset));
+                return sectionList;
+            }
+        })
     }
 
     public render() {
@@ -46,16 +78,3 @@ class Editor extends React.Component<IEditorProps, IEditorState> {
         this._content = elt;
     }
 }
-
-const mapStateToProps = (state: any) => {
-    return state;
-};
-
-const mapDispatchToProps = (dispatch: Dispatch<any>) => {
-    return {};
-};
-
-export default connect<{}, {}, IEditorProps>(
-    mapStateToProps,
-    mapDispatchToProps
-)(Editor);
